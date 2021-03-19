8172f00dc24cb85920316968a875af4ab77b4192
Lucie Fontaine <l.fontaine@meridian.internal>
2021-03-19 13:54:00 -0500

    CNPY-18 actions entry point

---
+++ projects/canopy-ui/src/lib/actions/public-api.ts
@@ -0,0 +1,6 @@
+export * from './button/button.component';
+export * from './button/button.module';
+export * from './icon-button/icon-button.component';
+export * from './icon-button/icon-button.module';
+export * from './menu/menu.component';
+export * from './menu/menu.module';
